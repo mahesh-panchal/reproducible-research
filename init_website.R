d1e90ad0a34870032f022cae197414128230dfdf
Mahesh Binzer-Panchal <mahesh.binzer-panchal@nbis.se>
2022-01-07 13:20:09 +0000

    Separate distill install from website init

--- init_website.R
+++
@@ -1,7 +0,0 @@
-# Run as root until custom image is made
-install.packages('distill')
-
-# Initialise the distill website 
-library('distill')
-create_website(dir = "gh-pages", title = "My reproducible research project")
-# Builds the website in the directory `gh-pages`. Change Github pages to render from this folder.
---
+++ scripts/init_website.R
@@ -0,0 +1,4 @@
+# Initialise the distill website 
+library('distill')
+create_website(dir = "website", title = "My reproducible research project")
+# Builds the website in the directory `website`. Change Github pages to render from this folder.
