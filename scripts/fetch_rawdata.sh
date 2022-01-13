0dc4312d8438024a7f237b414dfedfea340062c6
Mahesh Binzer-Panchal <mahesh.binzer-panchal@nbis.se>
2022-01-13 18:57:14 +0000

    Update fetch data script

--- scripts/fetch_rawdata.sh
+++ scripts/fetch_rawdata.sh
@@ -1,8 +1,13 @@
-mkdir -p /workspace/data/raw
-cd /workspace/data/raw
-wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/004/SRR2589044/SRR2589044_1.fastq.gz
-wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/004/SRR2589044/SRR2589044_2.fastq.gz
-wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/003/SRR2584863/SRR2584863_1.fastq.gz
-wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/003/SRR2584863/SRR2584863_2.fastq.gz
-wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/006/SRR2584866/SRR2584866_1.fastq.gz
-wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/006/SRR2584866/SRR2584866_2.fastq.gz
+DOWNLOAD_DIR=${DOWNLOAD_DIR:-/workspace/data/raw}
+mkdir -p "$DOWNLOAD_DIR"
+cd "$DOWNLOAD_DIR"
+wget -i - -nc << EOF
+ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/004/SRR2589044/SRR2589044_1.fastq.gz
+ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/004/SRR2589044/SRR2589044_2.fastq.gz
+ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/003/SRR2584863/SRR2584863_1.fastq.gz
+ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/003/SRR2584863/SRR2584863_2.fastq.gz
+ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/006/SRR2584866/SRR2584866_1.fastq.gz
+ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/006/SRR2584866/SRR2584866_2.fastq.gz
+EOF
+echo "\nData downloaded to ${DOWNLOAD_DIR}\n"
+ls -ldh "$DOWNLOAD_DIR"/*
